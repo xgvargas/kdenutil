1e6d12610959fa9d51cec208aee6542e7c2b67dd
Gustavo Vargas <xgvargas@gmail.com>
2021-05-28 17:41:48 -0300

    added stopwatches

---
+++ stopwatch/README.md
@@ -0,0 +1,37 @@
+# Stopwatch
+
+This is a not optimized way to generate precise up counter videos with a specific framerate.
+
+Since it is not intended to be used multiple times (you generate a video and thats it), the script is a simple bash script that need to be edited to set some variable before run.
+
+You must be capable of running a `bash` script and also **must** have in your path:
+
+- ffmpeg
+- inkscape
+- bc (the bash calculator)
+
+After script edition simple run `bash process.sh` and *wait... wait... wait...*
+
+Some standard stopwatches were already generated and made available [here](https://xgvargas.github.io/kdenutil/)
+
+## Variables
+
+There are only 3 variables to set:
+
+- `seconds` - the number of seconds to create the timer, like `60` for 1 minute, `300` for 5 minutes
+- `fps` - fps of final video, `29.97`, `30`, etc.
+- `full` - boolean to show complete clock or only the non 0 fields.
+    - `false` -> `12.123`
+    - `true` -> `0:00:12.123`
+
+## Style
+
+The best way to set your own style is to edit the `stopwatch.svg` file with some svg editor like `inkscape` and replace the original one.
+
+## Tips
+
+Remember that you will edit this video to overlay it on top of your original video. So, you don't need to generate different lengths videos, just create a single long one that counts up to a high number and then simple cut the end when doing the edition.
+
+But **you MUST generate different video for different final framerate**.
+
+If you are working with fps 29.97 then add a stopwatch with this specific framerate. if your video will be 30fps then used a 30fps stopwatch, came on, you got it right?
---
+++ stopwatch/process.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+##################################
+##################################
+##################################
+
+# somando 0.5 pode resolver o problem de arredondar pra CIMA
+
+#### fontes:
+# Ani            - sete segmentos
+# ubuntu mono    - melhor
+
+##################################
+##################################
+##################################
+
+
+
+seconds=$(bc <<< "scale=0; 60*1")
+fps=30
+full=false
+font="ubuntu mono"
+
+
+
+start=0
+end=$(bc <<< "scale=0; $fps * $seconds + 1")
+
+spf=$(bc <<< "scale=12; 1/$fps")
+
+echo 'SPF:' $spf $start $end
+
+
+for f in $(seq $start $end); do
+    c=$(bc <<< "scale=12; $f * $spf ")
+    h=$(bc <<< "scale=0; $c / (60*60)")
+    m=$(bc <<< "scale=0; ($c / 60) % 60")
+    s=$(bc <<< "scale=0; ($c / 1) % 60")
+    ms=$(bc <<< "scale=0; ((($c) * 1000) / 1) % 1000")
+    # echo $f '->' $c $h $m $s $ms
+
+    if [ $full = true ]; then
+        txt="$(printf '%d' $h):$(printf '%02d' $m):$(printf '%02d' $s).$(printf '%03d' $ms)"
+    else
+        if (( h > 0 )); then
+            txt="$(printf '%d' $h):$(printf '%02d' $m):$(printf '%02d' $s).$(printf '%03d' $ms)"
+        elif (( m > 0 )); then
+                                txt="$(printf '%d' $m):$(printf '%02d' $s).$(printf '%03d' $ms)"
+        else
+                                                    txt="$(printf '%d' $s).$(printf '%03d' $ms)"
+        fi
+    fi
+
+    echo $f $txt
+
+    sed "s/00:00:00.000/$txt/" stopwatch.svg > _tmp.svg
+
+    inkscape -o /tmp/frame-$(printf '%05d' $f).png _tmp.svg &> /dev/null
+done
+
+ffmpeg -framerate $fps -i /tmp/frame-%05d.png "stopwatch-${fps//./_}fps-${seconds}secs-${font// /_}".mp4
+
+rm /tmp/frame-*.png
